83ca0bb8d1360f01f45038bf37e39ea3bc5c373d
Thanh Duc Nguyen <thanh.nguyenduc@hellopay.com>
2016-04-18 11:30:09 +0700

    func -> func

--- swiftPracticing/main.swift
+++ swiftPracticing/main.swift
@@ -18,8 +18,6 @@ func fibonaci(num: Int) -> Int {
     }
 }
 
-print(fibonaci(15))
-
 func multitable(max: Int) -> Int {
     for var i = 1; i < max; i++ {
         for var j = 1; j < max; j++ {
@@ -30,4 +28,28 @@ func multitable(max: Int) -> Int {
     return 0
 }
 
-multitable(9)
+
+//Capturing value
+//Return a function that has no params and it returns Int
+
+func makeIncrementer(forIncrement amount: Int) -> () -> Int {
+    var runningTotal = 0
+    func incrementer() -> Int {
+        runningTotal += amount
+        return runningTotal
+    }
+    return incrementer
+}
+
+let incrementByTen = makeIncrementer(forIncrement: 10)
+
+print(incrementByTen())
+// returns a value of 10
+incrementByTen()
+// returns a value of 20
+incrementByTen()
+// returns a value of 30
+
+
+
+
